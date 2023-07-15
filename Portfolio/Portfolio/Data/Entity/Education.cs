0595e99aa91d2275c50e2c354fb33a67d6f4f0cf
SaraRasoulian <sara.rasolian@gmail.com>
2023-07-15 18:07:47 +0330

    entity properties added

--- Portfolio/Portfolio/Data/Entity/Education.cs
+++ Portfolio/Portfolio/Data/Entity/Education.cs
@@ -1,7 +1,27 @@
-﻿namespace Portfolio.Data.Entity
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Portfolio.Data.Entity
 {
     public class Education
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
+
+        [MaxLength(50)]
+        public required string Degree { get; set; }
+
+        [MaxLength(250)]
+        public required string FieldOfStudy { get; set; }
+
+        [MaxLength(250)]
+        public required string School { get; set; }        
+
+        [MaxLength(25)]
+        public required string StartYear { get; set; }
+
+        [MaxLength(25)]
+        public required string EndYear { get; set; }
+
+        [MaxLength(500)]
+        public required string Description { get; set; }
     }
 }
---
+++ Portfolio/Portfolio/Data/Entity/SEOSettings.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Portfolio.Data.Entity
+{
+    public class SEOSettings
+    {
+        public Guid Id { get; set; }
+
+        [MaxLength(100)]
+        public required string WebSiteTitle { get; set; }
+
+        [MaxLength(250)]
+        public string? MetaAuthor { get; set; }
+
+        [MaxLength(250)]
+        public string? MetaDescription { get; set; }
+
+        [MaxLength(250)]
+        public string? MetaKeywords { get; set; }
+
+        public byte[]? Favicon { get; set; }
+    }
+}
