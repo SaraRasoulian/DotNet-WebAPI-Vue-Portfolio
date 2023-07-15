3eed58fa511cbb9f0e5d86f1524fc35155e71419
SaraRasoulian <sara.rasolian@gmail.com>
2023-07-15 16:23:31 +0330

    profile properties added

--- Portfolio/Portfolio/Data/Entity/Profile.cs
+++ Portfolio/Portfolio/Data/Entity/Profile.cs
@@ -1,6 +1,27 @@
-﻿namespace Portfolio.Data.Entity
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Portfolio.Data.Entity
 {
     public class Profile
     {
+        public Guid Id { get; set; }
+
+        [MaxLength(50)]
+        public required string FisrtName { get; set; }
+
+        [MaxLength(50)]
+        public required string LastName { get; set; }
+
+        [MaxLength(320)]
+        [EmailAddress]
+        public required string Email { get; set; }
+
+        [MaxLength(100)]
+        public required string Title { get; set; }
+
+        [MaxLength(1000)]
+        public required string Summary { get; set; }
+
+        public byte[]? Photo { get; set; }
     }
 }
