cc844ccb3b5b26545c79ae725fd1086cfc2c5dc1
SaraRasoulian <sara.rasolian@gmail.com>
2023-07-25 23:30:11 +0330

    the rest of the Action Methods implemented

--- Portfolio/WebAPI/Controllers/Educations2Controller.cs
+++ Portfolio/WebAPI/Controllers/Educations2Controller.cs
@@ -30,5 +30,89 @@ namespace WebAPI.Controllers
                 return StatusCode(StatusCodes.Status500InternalServerError);
             }
         }
+
+        [HttpGet("{id:guid}")]
+        public async Task<IActionResult> Get([FromRoute] Guid id)
+        {
+            try
+            {
+                var result = await _unitOfWork.Education2.GetById(id);
+                if (result is null) return StatusCode(StatusCodes.Status204NoContent);
+                return Ok(result);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Education model)
+        {
+            try
+            {
+                if (model is null || !ModelState.IsValid) return BadRequest(ModelState);
+
+                var result = await _unitOfWork.Education2.Add(model);
+                await _unitOfWork.Save();
+
+                if (result is null) return StatusCode(StatusCodes.Status204NoContent, result);
+                return Ok(result);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<IActionResult> Put([FromRoute] Guid id, [FromBody] Education model)
+        {
+            try
+            {
+                if (model is null || model.Id != id || !ModelState.IsValid) return BadRequest(ModelState);
+
+                var toUpdate = await _unitOfWork.Education2.GetById(id);
+                if (toUpdate is null) return BadRequest(ModelState);
+
+                //To Do : create a service layer maybe? this code shouln't be here
+                toUpdate.Degree = model.Degree;
+                toUpdate.FieldOfStudy = model.FieldOfStudy;
+                toUpdate.School = model.School;
+                toUpdate.StartYear = model.StartYear;
+                toUpdate.EndYear = model.EndYear;
+                toUpdate.Description = model.Description;
+                toUpdate.LastUpdatedAt = DateTime.UtcNow;
+
+                var result = _unitOfWork.Education2.Update(toUpdate);
+                await _unitOfWork.Save();
+
+                if (result is null) return StatusCode(StatusCodes.Status204NoContent);
+                return Ok(result);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<IActionResult> Delete([FromRoute] Guid id)
+        {
+            try
+            {
+                var toDelete = await _unitOfWork.Education2.GetById(id);
+                if (toDelete is null) return StatusCode(StatusCodes.Status204NoContent);
+
+                _unitOfWork.Education2.Delete(toDelete);
+                await _unitOfWork.Save();
+
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
     }
 }
