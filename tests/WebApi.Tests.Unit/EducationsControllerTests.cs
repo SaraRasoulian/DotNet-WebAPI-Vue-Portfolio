31a6a6e07e4e9f68958db8f627f14958342b62af
SaraRasoulian <sara.rasolian@gmail.com>
2023-08-07 21:58:36 +0330

    unit tests

--- tests/WebApi.Tests.Unit/EducationsControllerTests.cs
+++ tests/WebApi.Tests.Unit/EducationsControllerTests.cs
@@ -97,10 +97,8 @@ namespace WebApi.Tests.Unit
         {
             // Arrange
             var existingId = Guid.NewGuid();
-
             var educationsRepositoryMock = new Mock<IEducationRepository>();
             educationsRepositoryMock.Setup(repo => repo.GetById(existingId)).ThrowsAsync(new Exception("Simulated exception"));
-
             var educationController = new EducationsController(educationsRepositoryMock.Object);
 
             // Act
@@ -118,12 +116,10 @@ namespace WebApi.Tests.Unit
         public async Task Post_Valid_Model_Should_Return_OkResult_With_Education()
         {
             // Arrange
-            var validModel = new EducationDTO { /* initialize valid properties */ };
-            var expectedResult = new EducationDTO { Id = Guid.NewGuid(), /* add other necessary properties */ };
-
+            var validModel = new EducationDTO { };
+            var expectedResult = new EducationDTO { Id = Guid.NewGuid(), };
             var educationsRepositoryMock = new Mock<IEducationRepository>();
             educationsRepositoryMock.Setup(repo => repo.Add(validModel)).ReturnsAsync(expectedResult);
-
             var educationController = new EducationsController(educationsRepositoryMock.Object);
 
             // Act
@@ -133,15 +129,13 @@ namespace WebApi.Tests.Unit
             var okResult = Assert.IsType<OkObjectResult>(result);
             var returnedEducation = Assert.IsType<EducationDTO>(okResult.Value);
             Assert.Equal(expectedResult.Id, returnedEducation.Id);
-            // Add other assertions for property values if needed
         }
 
         [Fact]
         public async Task Post_Invalid_Model_Should_Return_BadRequest()
         {
             // Arrange
-            var invalidModel = new EducationDTO { /* initialize invalid properties */ };
-
+            var invalidModel = new EducationDTO { };
             var educationsRepositoryMock = new Mock<IEducationRepository>();
             var educationController = new EducationsController(educationsRepositoryMock.Object);
             educationController.ModelState.AddModelError("PropertyName", "Some error message");
@@ -157,11 +151,7 @@ namespace WebApi.Tests.Unit
         public async Task Post_Null_Input_Should_Return_BadRequest()
         {
             // Arrange
-            //var validModel = new EducationDTO { /* initialize valid properties */ };
-
             var educationsRepositoryMock = new Mock<IEducationRepository>();
-            //educationsRepositoryMock.Setup(repo => repo.Add(validModel)).ReturnsAsync((EducationDTO)null);
-
             var educationController = new EducationsController(educationsRepositoryMock.Object);
 
             // Act
@@ -175,15 +165,155 @@ namespace WebApi.Tests.Unit
         public async Task Post_On_Exception_Should_Return_InternalServerError()
         {
             // Arrange
-            var validModel = new EducationDTO { /* initialize valid properties */ };
-
+            var validModel = new EducationDTO { };
             var educationsRepositoryMock = new Mock<IEducationRepository>();
             educationsRepositoryMock.Setup(repo => repo.Add(validModel)).ThrowsAsync(new Exception("Simulated exception"));
-
             var educationController = new EducationsController(educationsRepositoryMock.Object);
 
             // Act
             var result = await educationController.Post(validModel);
+
+            // Assert
+            var statusCodeResult = Assert.IsType<StatusCodeResult>(result);
+            Assert.Equal(StatusCodes.Status500InternalServerError, statusCodeResult.StatusCode);
+        }
+        #endregion
+
+        #region Put 
+        [Fact]
+        public async Task Put_ValidModel_Should_Return_OkResult_With_UpdatedEducation()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            var validModel = new EducationDTO { Id = id, };
+            var expectedResult = new EducationDTO { Id = id, };
+            var educationsRepositoryMock = new Mock<IEducationRepository>();
+            educationsRepositoryMock.Setup(repo => repo.Update(id, validModel)).ReturnsAsync(expectedResult);
+            var educationController = new EducationsController(educationsRepositoryMock.Object);
+
+            // Act
+            var result = await educationController.Put(id, validModel);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnedEducation = Assert.IsType<EducationDTO>(okResult.Value);
+            Assert.Equal(expectedResult.Id, returnedEducation.Id);
+        }
+
+        [Fact]
+        public async Task Put_InvalidModel_Should_Return_BadRequest()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            var invalidModel = new EducationDTO { Id = id, };
+            var educationsRepositoryMock = new Mock<IEducationRepository>();
+            var educationController = new EducationsController(educationsRepositoryMock.Object);
+            educationController.ModelState.AddModelError("PropertyName", "Some error message");
+
+            // Act
+            var result = await educationController.Put(id, invalidModel);
+
+            // Assert
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task Put_Id_Mismatch_Should_Return_BadRequest()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            var validModel = new EducationDTO { Id = Guid.NewGuid(), };
+            var educationsRepositoryMock = new Mock<IEducationRepository>();
+            var educationController = new EducationsController(educationsRepositoryMock.Object);
+
+            // Act
+            var result = await educationController.Put(id, validModel);
+
+            // Assert
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task Put_Null_Input_Should_Return_BadRequest()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            var educationsRepositoryMock = new Mock<IEducationRepository>();
+            var educationController = new EducationsController(educationsRepositoryMock.Object);
+
+            // Act
+            var result = await educationController.Put(id, null);
+
+            // Assert
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task Put_On_Exception_Should_Return_InternalServerError()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            var validModel = new EducationDTO { Id = id, };
+            var educationsRepositoryMock = new Mock<IEducationRepository>();
+            educationsRepositoryMock.Setup(repo => repo.Update(id, validModel)).ThrowsAsync(new Exception("Simulated exception"));
+            var educationController = new EducationsController(educationsRepositoryMock.Object);
+
+            // Act
+            var result = await educationController.Put(id, validModel);
+
+            // Assert
+            var statusCodeResult = Assert.IsType<StatusCodeResult>(result);
+            Assert.Equal(StatusCodes.Status500InternalServerError, statusCodeResult.StatusCode);
+        }
+        #endregion
+
+        #region Delete
+        [Fact]
+        public async Task Delete_ExistingId_Should_Return_OkResult_With_DeletedEducation()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            var expectedResult = new EducationDTO { Id = id, };
+            var educationsRepositoryMock = new Mock<IEducationRepository>();
+            educationsRepositoryMock.Setup(repo => repo.Delete(id)).ReturnsAsync(expectedResult);
+            var educationController = new EducationsController(educationsRepositoryMock.Object);
+
+            // Act
+            var result = await educationController.Delete(id);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnedEducation = Assert.IsType<EducationDTO>(okResult.Value);
+            Assert.Equal(expectedResult.Id, returnedEducation.Id);
+        }
+
+        [Fact]
+        public async Task Delete_NonExistingId_Should_Return_BadRequest()
+        {
+            // Arrange
+            var nonExistingId = Guid.NewGuid();
+            var educationsRepositoryMock = new Mock<IEducationRepository>();
+            educationsRepositoryMock.Setup(repo => repo.Delete(nonExistingId)).ReturnsAsync((EducationDTO)null);
+            var educationController = new EducationsController(educationsRepositoryMock.Object);
+
+            // Act
+            var result = await educationController.Delete(nonExistingId);
+
+            // Assert
+            Assert.IsType<BadRequestResult>(result);
+        }
+
+        [Fact]
+        public async Task Delete_On_Exception_Should_Return_InternalServerError()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            var educationsRepositoryMock = new Mock<IEducationRepository>();
+            educationsRepositoryMock.Setup(repo => repo.Delete(id)).ThrowsAsync(new Exception("Simulated exception"));
+            var educationController = new EducationsController(educationsRepositoryMock.Object);
+
+            // Act
+            var result = await educationController.Delete(id);
 
             // Assert
             var statusCodeResult = Assert.IsType<StatusCodeResult>(result);
