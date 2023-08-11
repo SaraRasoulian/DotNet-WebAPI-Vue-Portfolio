c6445345d50890c2d58874255168023ebf8a0df7
SaraRasoulian <sara.rasolian@gmail.com>
2023-08-11 16:19:45 +0330

    Specific Repository added

--- src/Domain/Interfaces/IEducationRepository.cs
+++ src/Domain/Interfaces/IEducationRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace Domain.Interfaces
+﻿using Domain.Entities;
+namespace Domain.Interfaces
 {
     public interface IEducationRepository
     {
+        Task<IEnumerable<Education>> GetAll();
+        Task<Education?> GetById(Guid id);
+        Task Add(Education model);
+        void Update(Education model);
+        void Delete(Education model);
     }
 }
--- src/Infrastructure/Repositories/EducationRepository.cs
+++ src/Infrastructure/Repositories/EducationRepository.cs
@@ -1,8 +1,41 @@
-﻿using Domain.Interfaces;
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Repositories
 {
     public class EducationRepository : IEducationRepository
     {
+        private readonly IPortfolioDbContext _dbContext;
+        public EducationRepository(IPortfolioDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<Education>> GetAll()
+        {
+            return await _dbContext.Educations.OrderByDescending(c => c.StartYear).ThenByDescending(c => c.EndYear).ToListAsync();
+        }
+
+        public async Task<Education?> GetById(Guid id)
+        {
+            return await _dbContext.Educations.FindAsync(id);
+        }
+
+        public async Task Add(Education model)
+        {
+            await _dbContext.Educations.AddAsync(model);
+        }
+
+        public void Update(Education model)
+        {
+            _dbContext.Educations.Update(model);
+        }
+
+        public void Delete(Education model)
+        {
+            _dbContext.Educations.Remove(model);
+        }
     }
 }
