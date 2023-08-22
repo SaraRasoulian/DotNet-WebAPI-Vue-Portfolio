e5d55415a928abb8e7fd92d4c54831841b68d630
SaraRasoulian <sara.rasolian@gmail.com>
2023-08-22 19:19:29 +0330

    unit test for GetById method

--- tests/Application.Tests/EducationServiceTests.cs
+++ tests/Application.Tests/EducationServiceTests.cs
@@ -9,7 +9,7 @@ namespace Application.Tests
     public class EducationServiceTests
     {
         [Fact]
-        public async Task GetAll_With_Data_Returns_List_Of_EducationDTO()
+        public async Task GetAll_With_Data_Returns_List_Of_EducationDto()
         {
             // Arrange
             var unitOfWork = A.Fake<IUnitOfWork>();
@@ -27,5 +27,25 @@ namespace Application.Tests
             Assert.IsAssignableFrom<IEnumerable<EducationDto>>(result);
             Assert.Equal(educationData.Count, result.Count());
         }
+
+        [Fact]
+        public async Task GetById_With_Data_Returns_EducationDto()
+        {
+            // Arrange
+            var unitOfWork = A.Fake<IUnitOfWork>();
+            var educationRepository = A.Fake<IEducationRepository>();
+            A.CallTo(() => unitOfWork.Education).Returns(educationRepository);
+            var educationId = Guid.NewGuid();
+            var educationData = A.Dummy<Education>();
+            A.CallTo(() => educationRepository.GetById(educationId)).Returns(educationData);
+            var educationService = new EducationService(unitOfWork);
+
+            // Act
+            var result = await educationService.GetById(educationId);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<EducationDto>(result);
+        }        
     }
 }
