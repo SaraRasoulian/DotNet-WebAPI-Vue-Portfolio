17f0396541838c8f1904d26e64873f3bef1be713
SaraRasoulian <sara.rasolian@gmail.com>
2023-08-30 17:44:33 +0330

    client app updated

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -8,16 +8,6 @@ module.exports = {
     'eslint:recommended',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
-  overrides: [
-    {
-      files: [
-        'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}'
-      ],
-      'extends': [
-        'plugin:cypress/recommended'
-      ]
-    }
-  ],
   parserOptions: {
     ecmaVersion: 'latest'
   }
