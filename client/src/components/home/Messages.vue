0ccdb6543f727b7d382bd74d1561936757868c51
Sara Rasoulian <sara.rasolian@gmail.com>
2023-09-27 21:12:50 +0330

    validation for send message form

--- client/src/components/home/Messages.vue
+++ client/src/components/home/Messages.vue
@@ -6,14 +6,25 @@
     <div>
       <form>
         <div class="row">
-          <div class="six columns">
-            <input type="text" placeholder="Your name" v-model="formData.name" required maxlength="70" />
+          <div class="six columns form-item">
+            <input type="text" placeholder="Your name" v-model="formData.name" maxlength="70" />
+            <span class="validation-error" v-for="error in v$.name.$errors" :key="error.$uid">
+              {{ error.$message }}
+            </span>
           </div>
-          <div class="six columns">
-            <input type="email" placeholder="Your email" v-model="formData.email" required maxlength="200" />
+          <div class="six columns form-item">
+            <input type="email" placeholder="Your email" v-model="formData.email" maxlength="200" />
+            <span class="validation-error" v-for="error in v$.email.$errors" :key="error.$uid">
+              {{ error.$message }}
+            </span>
           </div>
         </div>
-        <textarea placeholder="Hi Sara …" v-model="formData.content" required maxlength="1000"></textarea>
+        <div class="form-item">
+          <textarea placeholder="Hi Sara …" v-model="formData.content" maxlength="1000"></textarea>
+          <span class="validation-error" v-for="error in v$.content.$errors" :key="error.$uid">
+            {{ error.$message }}
+          </span>
+        </div>
         <div class="row">
           <div class="six columns">
             <button class="button send-button icon-rotate" v-on:click.prevent="Send">
@@ -32,6 +43,8 @@
 import messagesService from '@/services/messagesService'
 import { useToast } from 'vue-toastification'
 import { reactive } from 'vue'
+import useVuelidate from '@vuelidate/core';
+import { required, email } from '@vuelidate/validators';
 
 const formData = reactive({
   name: "",
@@ -39,11 +52,24 @@ const formData = reactive({
   content: ""
 });
 
+const rules = {
+  name: { required },
+  email: { required, email },
+  content: { required }
+}
+const v$ = useVuelidate(rules, formData)
+
 const Send = async () => {
+  const result = await v$.value.$validate();
+  if (!result)
+    return
+
   const toast = useToast()
   try {
     messagesService.create(formData).then(response => {
       ClearForm()
+      v$.value.$reset()
+
       if (response.status == 200)
         toast.success("Message sent. Thank you!")
     })
@@ -53,7 +79,7 @@ const Send = async () => {
   }
 };
 
-const ClearForm = async () => {
+const ClearForm = () => {
   formData.name = formData.email = formData.content = ''
 }
 </script>
