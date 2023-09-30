c308a0793aa83fd50324eb51b933b6dcb1f1c177
Sara Rasoulian <sara.rasolian@gmail.com>
2023-09-30 20:26:42 +0330

    data annotation for messageDto

--- server/src/Application/DTOs/MessageDto.cs
+++ server/src/Application/DTOs/MessageDto.cs
@@ -1,14 +1,19 @@
-﻿namespace Application.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.DTOs
 {
     public class MessageDto
     {
         public Guid Id { get; set; }
 
-        public required string Name { get; set; }
+        [Required]
+        public string Name { get; set; }
 
-        public required string Email { get; set; }
+        [Required]
+        public string Email { get; set; }
 
-        public required string Content { get; set; }
+        [Required]
+        public string Content { get; set; }
 
         public bool IsRead { get; set; }
 
