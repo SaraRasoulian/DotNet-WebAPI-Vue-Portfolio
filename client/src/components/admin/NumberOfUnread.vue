e36207303fdb7dbf87b54a9985e5097720c91808
Sara Rasoulian <sara.rasolian@gmail.com>
2023-10-03 18:54:13 +0330

    script setup instead of normal script

--- client/src/components/admin/NumberOfUnread.vue
+++ client/src/components/admin/NumberOfUnread.vue
@@ -2,25 +2,20 @@
   <h6 v-if="unread > 0" class="badge-wrapper"><span class="badge badge-unread">{{ unread }}</span></h6>
 </template>
 
-<script>
+<script setup>
 import messagesService from '@/services/messagesService'
-export default {
-  data() {
-    return {
-      unread: 0,
-    };
-  },
-  methods: {
-    async getUnRead() {
-      await messagesService.GetNumberOfUnread().then(response => {
-        this.unread = response.data
-      })
-    },
-  },
-  mounted() {
-    this.getUnRead()
-  }
+import { ref } from 'vue'
+import { onMounted } from 'vue'
+
+const unread = ref(0)
+
+async function getUnRead() {
+  await messagesService.GetNumberOfUnread().then(response => {
+    unread.value = response.data
+  })
 }
+
+onMounted(() => {
+  getUnRead()
+})
 </script>
-
-
