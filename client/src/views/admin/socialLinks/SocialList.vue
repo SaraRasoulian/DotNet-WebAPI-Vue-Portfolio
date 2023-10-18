f7943cbffdf42abc473930db472e535796f9dbb3
Sara Rasoulian <sara.rasolian@gmail.com>
2023-10-18 13:32:11 +0330

    social links list done

--- client/src/views/admin/socialLinks/SocialList.vue
+++ client/src/views/admin/socialLinks/SocialList.vue
@@ -6,7 +6,7 @@
           <div class="list-title">
             <h4>Social Links</h4>
             <div class="list-total">
-              <span>3</span>
+              <span>{{ list.length }}</span>
               <span>total</span>
             </div>
           </div>
@@ -24,94 +24,36 @@
           <!--list header-->
           <div class="list-header">
             <div class="row">
-              <div class="col-lg-4 col-md-4 col-sm-12"><span>Icon</span></div>
-              <div class="col-lg-4 col-md-4 col-sm-12"><span>Name</span></div>
-              <div class="col-lg-4 col-md-4 col-sm-12"><span>Link</span></div>
+              <div class="col-lg-3 col-md-3 col-sm-12"><span>Icon</span></div>
+              <div class="col-lg-3 col-md-3 col-sm-12"><span>Name</span></div>
+              <div class="col-lg-6 col-md-6 col-sm-12"><span>Link</span></div>
             </div>
             <div class="control-header">
               <span>Controls</span>
             </div>
           </div>
           <!--list items-->
-          <div>
+          <div v-for="item in list" :key="item.id">
             <div class="list-item">
               <div class="row">
                 <div class="col-lg-3 col-md-3 col-sm-12">
-                  <img src="@/assets/home/images/social/linkedin.svg" class="social-img" alt="LinkedIn">
+                  <img :src="item.icon" class="social-img" :alt="item.name">
                 </div>
-                <div class="col-lg-3 col-md-3 col-sm-12"><span>LinkedIn</span></div>
-                <div class="col-lg-6 col-md-6 col-sm-12"><span>https://www.linkedin.com/in/sara-rasoulian</span></div>
+                <div class="col-lg-3 col-md-3 col-sm-12"><span>{{ item.name }}</span></div>
+                <div class="col-lg-6 col-md-6 col-sm-12"><span>{{ item.url }}</span></div>
               </div>
               <div class="buttons-wrapper">
-                <router-link :to="{ name: 'view-social', params: { id: 1 } }" class="list-button" data-toggle="tooltip"
+                <router-link :to="{ name: 'view-social', params: { id: item.id } }" class="list-button" data-toggle="tooltip"
                   data-placement="top" title="View">
                   <img src="@/assets/admin/images/view.png" class="list-button-icon" alt="View">
                 </router-link>
 
-                <router-link :to="{ name: 'edit-social', params: { id: 1 } }" class="list-button" data-toggle="tooltip"
+                <router-link :to="{ name: 'edit-social', params: { id: item.id } }" class="list-button" data-toggle="tooltip"
                   data-placement="top" title="Edit">
                   <img src="@/assets/admin/images/edit.svg" class="list-button-icon" alt="Edit">
                 </router-link>
 
-                <router-link :to="{ name: 'delete-social', params: { id: 1 } }" class="list-button" data-toggle="tooltip"
-                  data-placement="top" title="Delete">
-                  <img src="@/assets/admin/images/delete.svg" class="list-button-icon" alt="Delete">
-                </router-link>
-              </div>
-            </div>
-            <hr class="line" />
-          </div>
-
-          <div>
-            <div class="list-item">
-              <div class="row">
-                <div class="col-lg-3 col-md-3 col-sm-12">
-                  <img src="@/assets/home/images/social/github.svg" class="social-img" alt="GitHub">
-                </div>
-                <div class="col-lg-3 col-md-3 col-sm-12"><span>GitHub</span></div>
-                <div class="col-lg-6 col-md-6 col-sm-12"><span>https://github.com/SaraRasoulian</span></div>
-              </div>
-              <div class="buttons-wrapper">
-                <router-link :to="{ name: 'view-social', params: { id: 1 } }" class="list-button" data-toggle="tooltip"
-                  data-placement="top" title="View">
-                  <img src="@/assets/admin/images/view.png" class="list-button-icon" alt="View">
-                </router-link>
-
-                <router-link :to="{ name: 'edit-social', params: { id: 1 } }" class="list-button" data-toggle="tooltip"
-                  data-placement="top" title="Edit">
-                  <img src="@/assets/admin/images/edit.svg" class="list-button-icon" alt="Edit">
-                </router-link>
-
-                <router-link :to="{ name: 'delete-social', params: { id: 1 } }" class="list-button" data-toggle="tooltip"
-                  data-placement="top" title="Delete">
-                  <img src="@/assets/admin/images/delete.svg" class="list-button-icon" alt="Delete">
-                </router-link>
-              </div>
-            </div>
-            <hr class="line" />
-          </div>
-
-          <div>
-            <div class="list-item">
-              <div class="row">
-                <div class="col-lg-3 col-md-3 col-sm-12">
-                  <img src="@/assets/home/images/social/email.svg" class="social-img" alt="Email">
-                </div>
-                <div class="col-lg-3 col-md-3 col-sm-12"><span>Email</span></div>
-                <div class="col-lg-6 col-md-6 col-sm-12"><span>sara.rasoulian.dev@gmail.com</span></div>
-              </div>
-              <div class="buttons-wrapper">
-                <router-link :to="{ name: 'view-social', params: { id: 1 } }" class="list-button" data-toggle="tooltip"
-                  data-placement="top" title="View">
-                  <img src="@/assets/admin/images/view.png" class="list-button-icon" alt="View">
-                </router-link>
-
-                <router-link :to="{ name: 'edit-social', params: { id: 1 } }" class="list-button" data-toggle="tooltip"
-                  data-placement="top" title="Edit">
-                  <img src="@/assets/admin/images/edit.svg" class="list-button-icon" alt="Edit">
-                </router-link>
-
-                <router-link :to="{ name: 'delete-social', params: { id: 1 } }" class="list-button" data-toggle="tooltip"
+                <router-link :to="{ name: 'delete-social', params: { id: item.id } }" class="list-button" data-toggle="tooltip"
                   data-placement="top" title="Delete">
                   <img src="@/assets/admin/images/delete.svg" class="list-button-icon" alt="Delete">
                 </router-link>
@@ -128,4 +70,16 @@
 
 <script setup>
 import AdminLayout from '@/layouts/admin/Layout.vue'
+import socialLinksService from '@/services/socialLinksService'
+import { onMounted, ref } from 'vue'
+
+const list = ref([])
+
+async function getList() {
+  await socialLinksService.getAll().then(response => {
+    list.value = response.data
+  })
+}
+
+onMounted(getList)
 </script>
