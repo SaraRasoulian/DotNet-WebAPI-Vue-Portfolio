1710653fe0e257b67dfbc36299e136608b72b70c
Sara Rasoulian <sara.rasolian@gmail.com>
2023-10-28 13:52:51 +0330

    social links in home

--- client/src/components/home/SocialLinks.vue
+++ client/src/components/home/SocialLinks.vue
@@ -7,28 +7,11 @@
         </div>
         <div class="section-content">
             <div class="social center-content">
-                <div class="social-item">
-                    <a href="#" target="_self" class="hvr-underline-from-left">
+                <div v-for="item in list" :key="item.id" class="social-item">
+                    <a :href="item.url" target="_self" class="hvr-underline-from-left">
                         <div class="social-item-inner">
-                            <img src="@/assets/home/images/social/linkedin.svg" class="social-item-img" alt="LinkedIn">
-                            <span class="social-title">LinkedIn</span>
-                        </div>
-                    </a>
-                </div>
-                <div class="social-item">
-                    <a href="#" target="_self">
-                        <div class="social-item-inner">
-                            <img src="@/assets/home/images/social/github.svg" class="social-item-img" alt="GitHub">
-                            <span class="social-title">GitHub</span>
-                        </div>
-                    </a>
-                </div>
-
-                <div class="social-item">
-                    <a href="#" target="_self">
-                        <div class="social-item-inner">
-                            <img src="@/assets/home/images/social/email.svg" class="social-item-img" alt="Email">
-                            <span class="social-title">Email</span>
+                            <img :src="item.icon" class="social-item-img" :alt="item.name">
+                            <span class="social-title">{{ item.name }}</span>
                         </div>
                     </a>
                 </div>
@@ -38,4 +21,16 @@
 </template>
 
 <script setup>
+import socialLinksService from '@/services/socialLinksService'
+import { onMounted, ref } from 'vue'
+
+const list = ref([])
+
+async function getList() {
+  await socialLinksService.getAll().then(response => {
+    list.value = response.data
+  })
+}
+
+onMounted(getList)
 </script>
