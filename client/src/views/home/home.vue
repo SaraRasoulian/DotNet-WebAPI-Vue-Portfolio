d68223b8fba4e5908e2528e91ddadf6c072475a3
Sara Rasoulian <sara.rasolian@gmail.com>
2023-10-29 20:20:57 +0330

    fade in effect

--- client/src/views/home/home.vue
+++ client/src/views/home/home.vue
@@ -4,15 +4,15 @@
             <div class="container">
                 <HomeHeader />
 
-                <Profile />
+                <Profile class="fade-in-on-scroll" />
 
-                <Experiences />
+                <Experiences class="fade-in-on-scroll" />
 
-                <Educations />
+                <Educations class="fade-in-on-scroll" />
 
-                <SocialLinks />
+                <SocialLinks class="fade-in-on-scroll" />
 
-                <SendMessage />
+                <SendMessage class="fade-in-on-scroll" />
             </div>
             <HomeFooter />
         </div>
@@ -26,11 +26,43 @@
 </style>
 
 <script setup>
-import Profile from '@/components/home/Profile.vue'
-import Experiences from '@/components/home/Experiences.vue'
-import Educations from '@/components/home/Educations.vue'
-import SocialLinks from '@/components/home/SocialLinks.vue'
-import SendMessage from '@/components/home/SendMessage.vue'
-import HomeHeader from '@/components/home/HomeHeader.vue'
-import HomeFooter from '@/components/home/HomeFooter.vue'
+import Profile from "@/components/home/Profile.vue"
+import Experiences from "@/components/home/Experiences.vue"
+import Educations from "@/components/home/Educations.vue"
+import SocialLinks from "@/components/home/SocialLinks.vue"
+import SendMessage from "@/components/home/SendMessage.vue"
+import HomeHeader from "@/components/home/HomeHeader.vue"
+import HomeFooter from "@/components/home/HomeFooter.vue"
+
+import { ref, onMounted, onBeforeUnmount } from "vue"
+
+const pageTop = ref(0)
+const pageBottom = ref(0)
+
+const tags = ref([])
+
+onMounted(() => {
+    window.addEventListener("scroll", checkVisibility)
+    checkVisibility()
+})
+
+onBeforeUnmount(() => {
+    window.removeEventListener("scroll", checkVisibility)
+})
+
+const checkVisibility = () => {
+    pageTop.value = window.pageYOffset || document.documentElement.scrollTop
+    pageBottom.value = pageTop.value + window.innerHeight
+
+    tags.value = document.querySelectorAll(".fade-in-on-scroll")
+
+    tags.value.forEach((tag) => {
+        const tagTop = tag.getBoundingClientRect().top + pageTop.value
+        if (tagTop < pageBottom.value) {
+            tag.classList.add("visible")
+        } else {
+            tag.classList.remove("visible")
+        }
+    })
+}
 </script>
