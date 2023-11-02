24df1856305cbf337b012143513ec7b51456d691
Sara Rasoulian <sara.rasolian@gmail.com>
2023-11-02 21:00:06 +0330

    scroll down in home page

--- client/src/components/home/HomeHeader.vue
+++ client/src/components/home/HomeHeader.vue
@@ -1,8 +1,9 @@
 <template>
     <div class="header">
-        <div class="text fade-in-text">
-            <h5 class="text-sm">SO NICE TO</h5>
+        <div class="header-text-wrapper fade-in-text">
+            <h5 class="text-sm">so nice to</h5>
             <h1 class="text-lg">Meet you</h1>
         </div>
+        <img src="@/assets/home/images/scroll-down.gif" class="scroll-down">
     </div>
 </template>
