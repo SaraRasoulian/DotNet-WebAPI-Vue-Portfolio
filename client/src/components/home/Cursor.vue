d011e51273e86d9ebbbb2f11d3298dce683b7d40
Sara Rasoulian <sara.rasolian@gmail.com>
2023-11-16 20:19:34 +0330

    custom cursor style

--- client/src/components/home/Cursor.vue
+++ client/src/components/home/Cursor.vue
@@ -1,66 +1,30 @@
 <template>
-    <div ref="cursorContainer" class="cursor-container">
-      <div ref="customCursor" class="custom-cursor"></div>
-    </div>
-  </template>
+  <div ref="cursorContainer" class="cursor-container">
+    <div ref="customCursor" class="custom-cursor"></div>
+  </div>
+</template>
   
-  <style>
-  body {
-    cursor: default;
-    margin: 0;
-    overflow: hidden;
-  }
-  
-  .cursor-container {
-    position: fixed;
-    top: 0;
-    left: 0;
-    pointer-events: none;
-  }
-  
-  .custom-cursor {
-    width: 40px;
-    height: 40px;
-    border-radius: 50%;
+<script setup>
+import { ref, onMounted } from 'vue';
 
-  background: radial-gradient(closest-side, var(--top-left-color), transparent);
- 
-  animation: expandAndContract1 4s infinite alternate;
-}
-@keyframes expandAndContract1 {
-  0% {
-    width: 40px;
-    height: 40px;
-  }
-  100% {
-    width: 50px;
-    height: 50px;
-  }
-}
+const cursorContainer = ref(null);
+const customCursor = ref(null);
 
-  </style>
-  
-  <script setup>
-  import { ref, onMounted } from 'vue';
-  
-  const cursorContainer = ref(null);
-  const customCursor = ref(null);
-  
-  const positionElement = (e) => {
-    const mouseY = e.clientY -12;
-    const mouseX = e.clientX -12;
-  
-    customCursor.value.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
-  };
-  
-  onMounted(() => {
-    // Get initial mouse position to set the initial position of the cursor
-    const initialMouseX = window.innerWidth / 2;
-    const initialMouseY = window.innerHeight / 2;
-  
-    customCursor.value.style.transform = `translate3d(${initialMouseX}px, ${initialMouseY}px, 0)`;
-  
-    window.addEventListener('mousemove', positionElement);
-  });
-  </script>
+const positionElement = (e) => {
+  const mouseY = e.clientY - 16;
+  const mouseX = e.clientX - 16;
+
+  customCursor.value.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+};
+
+onMounted(() => {
+  // Get initial mouse position to set the initial position of the cursor
+  const initialMouseX = window.innerWidth / 2;
+  const initialMouseY = window.innerHeight / 2;
+
+  customCursor.value.style.transform = `translate3d(${initialMouseX}px, ${initialMouseY}px, 0)`;
+
+  window.addEventListener('mousemove', positionElement);
+});
+</script>
   
