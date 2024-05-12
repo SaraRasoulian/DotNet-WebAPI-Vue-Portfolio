49b86a01c9445c49096293d52e320e9d2895a9f7
sara rasoulian <sara.rasoulian.dev@gmail.com>
2024-05-12 15:47:50 +0330

    Set up WebApplicationFactory class for integration testing

---
+++ server/tests/WebApi.Tests.Integration/IntegrationTestWebApplicationFactory.cs
@@ -0,0 +1,56 @@
+﻿using Infrastructure.DbContexts;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Testcontainers.PostgreSql;
+
+namespace WebApi.Tests.Integration;
+
+public class IntegrationTestWebApplicationFactory : WebApplicationFactory<Program>, IAsyncLifetime
+{
+    private readonly PostgreSqlContainer _dbContainer = new PostgreSqlBuilder()
+        .WithImage("postgres:latest")
+        .WithDatabase("PortfolioDb_Test")
+        .WithUsername("postgres")
+        .WithPassword("mysecretpassword")
+        .Build();
+
+    public async Task InitializeAsync()
+    {
+        await _dbContainer.StartAsync();
+
+        using (var scope = Services.CreateScope())
+        {
+            var scopedServices = scope.ServiceProvider;
+            var cntx = scopedServices.GetRequiredService<PortfolioDbContext>();
+
+            await cntx.Database.EnsureCreatedAsync();
+        }
+    }
+
+    public new async Task DisposeAsync()
+    {
+        await _dbContainer.StopAsync();
+    }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(services =>
+        {
+            var descriptor = services.SingleOrDefault(s => s.ServiceType == typeof(DbContextOptions<PortfolioDbContext>));
+
+            if (descriptor is not null)
+            {
+                services.Remove(descriptor);
+            }
+
+            services.AddDbContext<PortfolioDbContext>(options =>
+            {
+                options.UseNpgsql(_dbContainer.GetConnectionString());
+            });
+        });
+    }
+}
+
