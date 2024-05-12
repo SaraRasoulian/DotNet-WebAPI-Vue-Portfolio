d09e502c1585180717f5a764449a0360afe97c2b
sara rasoulian <sara.rasoulian.dev@gmail.com>
2024-05-12 15:48:43 +0330

    Add integration test for educations controller

---
+++ server/tests/WebApi.Tests.Integration/ControllersTests/EducationsControllerTests.cs
@@ -0,0 +1,19 @@
+﻿namespace WebApi.Tests.Integration.ControllersTests;
+
+public class EducationsControllerTests : BaseControllerTest
+{
+    public EducationsControllerTests(IntegrationTestWebApplicationFactory factory) : base(factory)
+    {
+    }
+
+    [Fact]
+    public async Task GetAll_Returns_OK()
+    {
+        // Arrange
+        // Act
+        var response = await _httpClient.GetAsync("api/educations");
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+    }
+}
