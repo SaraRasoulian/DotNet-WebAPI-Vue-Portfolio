5eec1641e6c375b9b19faf75a6330bdc58fd2a45
sara rasoulian <sara.rasoulian.dev@gmail.com>
2024-05-12 15:48:09 +0330

    Create BaseControllerTest class

---
+++ server/tests/WebApi.Tests.Integration/BaseControllerTest.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApi.Tests.Integration;
+
+public class BaseControllerTest : IClassFixture<IntegrationTestWebApplicationFactory>
+{
+    protected readonly HttpClient _httpClient;
+
+    protected BaseControllerTest(IntegrationTestWebApplicationFactory factory)
+    {
+        _httpClient = factory.CreateDefaultClient();
+    }
+}
