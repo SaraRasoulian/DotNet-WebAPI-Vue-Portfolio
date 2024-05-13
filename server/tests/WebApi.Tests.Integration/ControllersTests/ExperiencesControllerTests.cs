29ddd47c670d7713d4daa050d1590c3512fd6145
sara rasoulian <sara.rasoulian.dev@gmail.com>
2024-05-13 12:44:18 +0330

    Add integration test for get all experiences api

---
+++ server/tests/WebApi.Tests.Integration/ControllersTests/ExperiencesControllerTests.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApi.Tests.Integration.ControllersTests;
+
+public class ExperiencesControllerTests : BaseControllerTest
+{
+    public ExperiencesControllerTests(IntegrationTestWebApplicationFactory factory) : base(factory)
+    { }
+
+    [Fact]
+    public async Task GetAll_Returns_SuccessStatusCode()
+    {
+        // Act
+        var response = await _httpClient.GetAsync("api/experiences");
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+    }
+}
